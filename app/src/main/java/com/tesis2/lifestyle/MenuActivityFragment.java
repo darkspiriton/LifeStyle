0dd893c34a788eead7e1cdbd98f930390b9b3ace
Cesar Moreno <cesar200526@gmail.com>
2015-11-04 10:45:51 -0500

    Agregado nivel de riesgo en el menu principal y boton en la patalla de resultado. Falta completar la pantalla de Resultado y mejorar el diseño y rendimiento de consulta.

--- app/src/main/java/com/tesis2/lifestyle/MenuActivityFragment.java
+++ app/src/main/java/com/tesis2/lifestyle/MenuActivityFragment.java
@@ -1,22 +1,52 @@
 package com.tesis2.lifestyle;
 
-import android.support.v4.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MenuActivityFragment extends Fragment {
-
+    TextView resultado;
     public MenuActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_menu, container, false);
+        obtenerResultado(rootView);
+
+        return rootView;
+    }
+    private void obtenerResultado(View rootView){
+
+
+        resultado = (TextView)rootView.findViewById(R.id.tvRiesgoResumen);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("RiskLevel");
+        query.whereEqualTo("level", ParseUser.getCurrentUser().getInt("riskLevel"));
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (object == null) {
+
+                } else {
+                    resultado.setText(object.getString("name").toUpperCase());
+                    resultado.setTextColor(Color.parseColor(object.getString("Color")));
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/tesis2/lifestyle/ResultadoActivity.java
+++ app/src/main/java/com/tesis2/lifestyle/ResultadoActivity.java
@@ -1,8 +1,11 @@
 package com.tesis2.lifestyle;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.parse.GetCallback;
@@ -11,25 +14,28 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class ResultadoActivity extends AppCompatActivity {
+public class ResultadoActivity extends AppCompatActivity implements View.OnClickListener {
 
-    static TextView resultado,porcentaje,puntos,descripcion;
-
+    TextView resultado,porcentaje,puntos,descripcion;
+    Button butMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+        obtenerResultado();
+        butMenu = (Button) findViewById(R.id.butRegresarMenu);
 
+        butMenu.setOnClickListener(this);
     }
 
-    @Override
-    public void onStart(){
-        super.onStart();
+    private void obtenerResultado(){
+
 
         resultado = (TextView) findViewById(R.id.tvRiesgo);
         porcentaje = (TextView) findViewById(R.id.porcentaje);
         puntos = (TextView) findViewById(R.id.puntos);
         descripcion = (TextView) findViewById(R.id.descripcion);
+
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("RiskLevel");
         query.whereEqualTo("level", ParseUser.getCurrentUser().getInt("riskLevel"));
@@ -46,8 +52,12 @@ public class ResultadoActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 
 
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this,MenuActivity.class);
+        startActivity(intent);
+    }
 }
